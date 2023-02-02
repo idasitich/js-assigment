db1d890115576a6d4e3d7cd4768fe4649f6ec03d
idasitich <karris.kk@gmail.com>
2023-02-02 07:45:37 +0700

    little assignments ready

--- assignment_1.js
+++ assignment_1.js
@@ -1,38 +1,29 @@
 //Mathematical Shapes
+//square
+const squareDiagonal = side => side*Math.sqrt(2);
 
-const myCircleRadio = 4;
-const myTriangleSides = { first: 5, second: 6, third:7} ;
+const triangleArea = (a,b,c) => {
+    const s = (a + b + c )/2;
+    const area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+    return s, area;
+};
 
-//find the area of a triangle
-
-// const triangleArea = (a, b, c) =>{
-
-// }
-
-//circumference and area of a circle
-
-const circleProducts = (radio) => {
+const circleAreaAndCircleCircumference = (radio) => {
     const circumference = 2 * radio * Math.PI ; //2 pi rad
     const area = radio * radio * Math.PI; //r2 pi
-
+    console.log(`the area of a circle with a radio of 4 is ${area} and the circumference is ${circumference}`);
     return {
         circumference,
         area,
     }
+};
+
+const thiscircleAreaAndCircleCircumference = () => circleAreaAndCircleCircumference(4);
+
+const littleAssignments = () => {
+    console.log(`the diagonal of a square where the length of each side is 9, is ${squareDiagonal(9)}`);
+    console.log(`the area of a triangle where the length of three of its sides are 5,6,7 is ${triangleArea(5,6,7)}`);
+    thiscircleAreaAndCircleCircumference();
 }
 
-
-
-const thisCircleProducts = () => circleProducts(myCircleRadio);
-
-console.log(thisCircleProducts());
-
-
-
-const circumference = thisCircleProducts().circumference;
-const area = thisCircleProducts().area;
-
-console.log('only circumference', circumference);
-
-console.log('that is what out method returns', circleProducts(3));
-console.log('thats what thisCircleProduct returns', thisCircleProducts());
+littleAssignments();
