efc3aa9d98d875b38d4c7f1e5e840aa45760e576
idasitich <karris.kk@gmail.com>
2023-02-04 07:34:11 +0700

    game ready

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const computerPlay = () => {
     const value = Math.random();
-    let word= "paper";
-    if(value < 0.33) {
-        word = "scissors";
-    } else if ( 0.33 <= value && value < 0.66 ) {
-        word = "rock";
-    }; 
-    console.log('computer', word);
+    let word = value < 0.33? "Scissors": (0.33 <= value && value < 0.66 )? "Rock": "Paper";
     return word;
 };
 
+const wordCapitalized = (str) => {
+    str.trim().toLowerCase();
+    const firstLetter = str.charAt(0).toUpperCase();
+    const remainingLetters = str.substring(1,);
+    const capitalizedWord = firstLetter + remainingLetters;
+    return capitalizedWord;
+};
+
 const userPlay = () => {
-    let word = prompt("Enter your choice and prepare to die :D")
-    console.log('word', word);
-    let cleanWord = word.trim().toLowerCase();
-    console.log('cleanword', cleanWord);
-    while(cleanWord != 'rock' && cleanWord != 'scissors' &&  cleanWord != 'paper'){
-        let wordy = prompt("Don't mess with me (enter only Rock, Paper or Scissors)");
-        console.log('wordy', wordy)
-        cleanWord = wordy.trim().toLowerCase();
+    let rawWord = prompt("Enter your choice and prepare to die :D");
+    let word = wordCapitalized(rawWord); 
+    while(word != 'Rock' && word != 'Scissors' &&  word != 'Paper'){
+        let newWord = prompt("Don't mess with me (enter only Rock, Paper or Scissors)");
+        word = wordCapitalized(newWord);
     }
-    console.log('user', cleanWord);
-    return cleanWord;
+    return word;
 };
 
 const playRound = (playerSelection, computerSelection) => {
@@ -29,24 +27,24 @@ const playRound = (playerSelection, computerSelection) => {
     let userCounter = 0;
     let computerCounter = 1;
 
-    if( playerSelection == 'rock' && computerSelection=='scissors') {
+    if( playerSelection == 'Rock' && computerSelection=='Scissors') {
         result = 'Rock breaks Scissors, you win this round !';
         userCounter = 1;
         computerCounter = 0;
     } 
-    if ( playerSelection == 'scissors' && computerSelection=='paper' ) {
-        result = 'Scissors cut paper, you win this round !';
+    if ( playerSelection == 'Scissors' && computerSelection=='Paper' ) {
+        result = 'Scissors cut Paper, you win this round !';
         userCounter = 1;
         computerCounter = 0;
     } 
-    if (playerSelection == 'paper' && computerSelection=='rock') {
-        result = 'Paper wraps the rock, you win this round !';
+    if (playerSelection == 'Paper' && computerSelection=='Rock') {
+        result = 'Paper wraps the Rock, you win this round !';
         userCounter = 1;
         computerCounter = 0;
     } 
-    if ((playerSelection == 'paper' && computerSelection=='paper') ||
-        (playerSelection == 'rock' && computerSelection=='rock') || 
-        (playerSelection == 'scissors' && computerSelection=='scissors')) {
+    if ((playerSelection == 'Paper' && computerSelection=='Paper') ||
+        (playerSelection == 'Rock' && computerSelection=='Rock') || 
+        (playerSelection == 'Scissors' && computerSelection=='Scissors')) {
         result = 'It is a tie !';
         userCounter = 0;
         computerCounter = 0;
@@ -59,41 +57,44 @@ const playRound = (playerSelection, computerSelection) => {
 };
 
 const game = () => {
+    alert("Welcome to Rock, Scissors, and Paper game !!!")
     let computerCounter = 0;
     let userCounter = 0;
-    let winner = () => { for(let i=0; i<5; i++){
+    const winner = () => { 
+        for(let i=0; i<5; i++){
             let computer = computerPlay();
             let user = userPlay();
             let roundResult = playRound(user, computer);
             userCounter += roundResult.userCounter;
             computerCounter+= roundResult.computerCounter;
-
             window.alert(`
             Player : ${user}, 
             Computer : ${computer} 
             ${roundResult.result} 
             Player points: ${userCounter} | Computer points: ${computerCounter}`)
         }
-    }
+    };
+
     winner();
-        if (computerCounter > userCounter) {
-            window.alert(`
-            Final Score: 
-            Computer : ${computerCounter} |  Player : ${userCounter}
-        
-            YOU LOSE ! TRY AGAIN HAHAHA 
-            so you can bite the dust hahahahahahah`
-            )
-        } else if (computerCounter < userCounter){
-            window.alert(`
-            Final Score: 
-            Computer : ${computerCounter} | layer : ${userCounter}
-        
-            YOU WIN ! CONGRATULATION.`
-            )
-        }else{
-            window.alert("You both are a tie, Play again ! FIGHTING !!!")
-        }
+    
+    if (computerCounter > userCounter) {
+        window.alert(`
+        Final Score: 
+        Computer : ${computerCounter} |  Player : ${userCounter}
+    
+        YOU LOSE ! TRY AGAIN HAHAHA 
+        so you can bite the dust hahahahahahah`
+        )
+    } else if (computerCounter < userCounter){
+        window.alert(`
+        Final Score: 
+        Computer : ${computerCounter} | layer : ${userCounter}
+    
+        YOU WIN ! CONGRATULATION.`
+        )
+    }else{
+        window.alert("You both are a tie, Play again ! FIGHTING !!!")
+    }
 };
 
 game();
